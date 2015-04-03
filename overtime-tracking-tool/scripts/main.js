85ab80adbfc005cfb80c44f9ecc25ffe42263a18
cminhho <hmchung92@gmail.com>
2015-04-03 20:56:14 +0700

    update overtime-tracking-tool template

---
+++ overtime-tracking-tool/scripts/main.js
@@ -0,0 +1,5 @@
+$('document').ready(function() {
+    $('#ott-header').load('./templates/header.html');
+    $('#oot-mainnav').load('./templates/mainnav.html');
+    $('#ott-footer').load('./templates/footer.html');
+});
